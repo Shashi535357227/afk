9271cc2eec6659b5d2eee6fe3fb41d153b677ad1
Shashi535357227 <149998207+Shashi535357227@users.noreply.github.com>
2024-03-19 09:55:39 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,130 @@
+import sys
+import heroku3
+
+from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, OWNER_ID, SUDO_USERS, HEROKU_APP_NAME, HEROKU_API_KEY, CMD_HNDLR as hl
+
+from os import execl, getenv
+from telethon import events
+from datetime import datetime
+
+
+@X1.on(events.NewMessage(incoming=True, pattern=r"\%sping(?: |$)(.*)" % hl))
+@X2.on(events.NewMessage(incoming=True, pattern=r"\%sping(?: |$)(.*)" % hl))
+@X3.on(events.NewMessage(incoming=True, pattern=r"\%sping(?: |$)(.*)" % hl))
+@X4.on(events.NewMessage(incoming=True, pattern=r"\%sping(?: |$)(.*)" % hl))
+@X5.on(events.NewMessage(incoming=True, pattern=r"\%sping(?: |$)(.*)" % hl))
+@X6.on(events.NewMessage(incoming=True, pattern=r"\%sping(?: |$)(.*)" % hl))
+@X7.on(events.NewMessage(incoming=True, pattern=r"\%sping(?: |$)(.*)" % hl))
+@X8.on(events.NewMessage(incoming=True, pattern=r"\%sping(?: |$)(.*)" % hl))
+@X9.on(events.NewMessage(incoming=True, pattern=r"\%sping(?: |$)(.*)" % hl))
+@X10.on(events.NewMessage(incoming=True, pattern=r"\%sping(?: |$)(.*)" % hl))
+async def ping(e):
+    if e.sender_id in SUDO_USERS:
+        start = datetime.now()
+        altron = await e.reply(f"😈☠️𝓃𝒶𝔨𝕦ᒪ'ｓ😈 ⒻⓊς𝓀𝓔Ř ｂσｔ☠️😈")
+        end = datetime.now()
+        mp = (end - start).microseconds / 1000
+        await altron.edit(f"𝐊ʏα 𝐎яƉɛя 𝐇αι MALIK")
+
+
+@X1.on(events.NewMessage(incoming=True, pattern=r"\%sstop(?: |$)(.*)" % hl))
+@X2.on(events.NewMessage(incoming=True, pattern=r"\%sstop(?: |$)(.*)" % hl))
+@X3.on(events.NewMessage(incoming=True, pattern=r"\%sstop(?: |$)(.*)" % hl))
+@X4.on(events.NewMessage(incoming=True, pattern=r"\%sstop(?: |$)(.*)" % hl))
+@X5.on(events.NewMessage(incoming=True, pattern=r"\%sstop(?: |$)(.*)" % hl))
+@X6.on(events.NewMessage(incoming=True, pattern=r"\%sstop(?: |$)(.*)" % hl))
+@X7.on(events.NewMessage(incoming=True, pattern=r"\%sstop(?: |$)(.*)" % hl))
+@X8.on(events.NewMessage(incoming=True, pattern=r"\%sstop(?: |$)(.*)" % hl))
+@X9.on(events.NewMessage(incoming=True, pattern=r"\%sstop(?: |$)(.*)" % hl))
+@X10.on(events.NewMessage(incoming=True, pattern=r"\%sstop(?: |$)(.*)" % hl))
+async def stop(e):
+    if e.sender_id in SUDO_USERS:
+        await e.reply(f"`𝚂𝚃𝙾𝙿𝙸𝙽𝙶  ʙᴏᴛ...`")
+        try:
+            await X1.disconnect()
+        except Exception:
+            pass
+        try:
+            await X2.disconnect()
+        except Exception:
+            pass
+        try:
+            await X3.disconnect()
+        except Exception:
+            pass
+        try:
+            await X4.disconnect()
+        except Exception:
+            pass
+        try:
+            await X5.disconnect()
+        except Exception:
+            pass
+        try:
+            await X6.disconnect()
+        except Exception:
+            pass
+        try:
+            await X7.disconnect()
+        except Exception:
+            pass
+        try:
+            await X8.disconnect()
+        except Exception:
+            pass
+        try:
+            await X9.disconnect()
+        except Exception:
+            pass
+        try:
+            await X10.disconnect()
+        except Exception:
+            pass
+
+        execl(sys.executable, sys.executable, *sys.argv)
+
+
+@X1.on(events.NewMessage(incoming=True, pattern=r"\%ssudo(?: |$)(.*)" % hl))
+@X2.on(events.NewMessage(incoming=True, pattern=r"\%ssudo(?: |$)(.*)" % hl))
+@X3.on(events.NewMessage(incoming=True, pattern=r"\%ssudo(?: |$)(.*)" % hl))
+@X4.on(events.NewMessage(incoming=True, pattern=r"\%ssudo(?: |$)(.*)" % hl))
+@X5.on(events.NewMessage(incoming=True, pattern=r"\%ssudo(?: |$)(.*)" % hl))
+@X6.on(events.NewMessage(incoming=True, pattern=r"\%ssudo(?: |$)(.*)" % hl))
+@X7.on(events.NewMessage(incoming=True, pattern=r"\%ssudo(?: |$)(.*)" % hl))
+@X8.on(events.NewMessage(incoming=True, pattern=r"\%ssudo(?: |$)(.*)" % hl))
+@X9.on(events.NewMessage(incoming=True, pattern=r"\%ssudo(?: |$)(.*)" % hl))
+@X10.on(events.NewMessage(incoming=True, pattern=r"\%ssudo(?: |$)(.*)" % hl))
+async def addsudo(event):
+    if event.sender_id == OWNER_ID:
+        Heroku = heroku3.from_key(HEROKU_API_KEY)
+        sudousers = getenv("SUDO_USERS", default=None)
+
+        ok = await event.reply(f"» __ᴀᴅᴅɪɴɢ SPAM ʙᴏᴛ sᴜᴅᴏ....__")
+        target = ""
+        if HEROKU_APP_NAME is not None:
+            app = Heroku.app(HEROKU_APP_NAME)
+        else:
+            await ok.edit("`[HEROKU]:" "\nPlease Setup Your` **HEROKU_APP_NAME**")
+            return
+        heroku_var = app.config()
+        if event is None:
+            return
+        try:
+            reply_msg = await event.get_reply_message()
+            target = reply_msg.sender_id
+        except:
+            await ok.edit("» ᴛᴀɢ ᴋᴀʀ ᴋᴇ ᴋᴀʀ !!")
+            return
+
+        if str(target) in sudousers:
+            await ok.edit(f"ᴛʜɪꜱ ᴜꜱᴇʀ ɪꜱ ᴀʟʀᴇᴀᴅʏ ᴀ  sᴘᴀᴍ ʙᴏᴛ ꜱᴜᴅᴏ ᴜꜱᴇʀ !!")
+        else:
+            if len(sudousers) > 0:
+                newsudo = f"{sudousers} {target}"
+            else:
+                newsudo = f"{target}"
+            await ok.edit(f"» **ɴᴇᴡ ꜱᴜᴅᴏ ᴜꜱᴇʀ**: `{target}`\n» `ᴡᴀɪᴛ ᴋᴀʀ ʙʜᴀɪ BOT sᴜʀᴜ ʜᴏ ʀʜᴀ ʜᴀɪ...`")
+            heroku_var["SUDO_USERS"] = newsudo    
+    
+    elif event.sender_id in SUDO_USERS:
+        await event.reply("» AUKAT ME REH MADHARCHOD ")
---
+++ start.py
@@ -0,0 +1,50 @@
+from telethon import __version__, events, Button
+
+from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10
+
+
+START_BUTTON = [
+    [
+       
+        Button.url("𝐎ᴡɴᴇʀ", "https://t.me/@Yours_innocent_boy")
+    ],
+    [
+        Button.url(" 𝐂н𝙰𝙽𝙽𝙴𝙻 ", "https://t.me/THE_FUKER_BOT_2926"),
+        Button.url(" 𝐒𝚄𝙿𝙿𝙾𝚃  ", "https://t.me/THE_FUCKING_BOTS_2926")
+    ],
+    [
+        Button.url("𝐑ᴇᴘᴏ ", "KHUD KA BNA LE BSDK"),
+      
+        Button.url(" DEVELOPER ", "https://t.me/Rajput_hu"),
+        
+    ]
+]
+
+
+@X1.on(events.NewMessage(pattern="/start"))
+@X2.on(events.NewMessage(pattern="/start"))
+@X3.on(events.NewMessage(pattern="/start"))
+@X4.on(events.NewMessage(pattern="/start"))
+@X5.on(events.NewMessage(pattern="/start"))
+@X6.on(events.NewMessage(pattern="/start"))
+@X7.on(events.NewMessage(pattern="/start"))
+@X7.on(events.NewMessage(pattern="/start"))
+@X8.on(events.NewMessage(pattern="/start"))
+@X9.on(events.NewMessage(pattern="/start"))
+@X10.on(events.NewMessage(pattern="/start"))
+async def start(event):              
+    if event.is_private:
+        AltBot = await event.client.get_me()
+        bot_name = AltBot.first_name
+        bot_id = AltBot.id
+        TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
+        TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [SUPRA](https://t.me/Rajput_hu)**\n\n"
+        TEXT += f"» **ᴅᴇᴀᴅ ᴠᴇʀsɪᴏɴ :** `M3.3`\n"
+        TEXT += f"» **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `3.11.3`\n"
+        TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{__version__}`\n━━━━━━━━━━━━━━━━━"
+        await event.client.send_file(
+                    event.chat_id,
+                    "https://telegra.ph/file/2869b59ce23e2a4674560.jpg",
+                    caption=TEXT, 
+                    buttons=START_BUTTON
+)
